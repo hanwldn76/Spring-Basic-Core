3f40a4dd13f506ab7218664c65b2552b506228f7
hanwldn76 <hanwldn76@gmail.com>
2024-04-12 23:27:22 +0900

    Section 7. 의존관계 자동 주입

--- src/main/java/hello/core/AutoAppConfig.java
+++ src/main/java/hello/core/AutoAppConfig.java
@@ -12,8 +12,9 @@ import org.springframework.context.annotation.FilterType;
         excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class)
 )
 public class AutoAppConfig {
-    @Bean(name = "memoryMemberRepository")
-    MemberRepository MemoryMemberRepository(){
-        return new MemoryMemberRepository();
-    }
+    // 스프링 빈에 컴포넌트 스캔으로 등록을 했는데 또 수동으로 등록해서 오류남 -> 주석 처리
+//    @Bean(name = "memoryMemberRepository")
+//    MemberRepository MemoryMemberRepository(){
+//        return new MemoryMemberRepository();
+//    }
 }
